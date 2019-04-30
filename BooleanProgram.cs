99d1d4ddfb21aa30538299797449d42395103ba5
Tyler <hybound@hotmail.com>
2019-04-30 11:28:00 -0600

    Create BooleanProgram.cs

---
+++ BooleanProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BooleanProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool result = false;
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            if (age > 14 && dui == false && tickets < 3)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            Console.WriteLine("Qualified to receive insurance?");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}

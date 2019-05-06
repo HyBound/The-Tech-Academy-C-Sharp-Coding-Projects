3376f52fe71786de73339f78a4e7104528b249a8
Tyler <hybound@hotmail.com>
2019-05-06 11:45:59 -0600

    Create ArrayProgram

---
+++ ArrayProgram.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            String[] words = { "cobble", "wobble", "bobble", "tobble", "mobble" };
+            int[] numbers = { 7, 22, 83, 63, 21, 3 };
+            List<string> stList = new List<string>();
+            stList.Add("Apple");
+            stList.Add("Pear");
+            stList.Add("Pineapple");
+            stList.Add("Cucumber");
+            int wrdIndex = 0;
+            int numIndex = 0;
+            int listIndex = 0;
+
+            Console.WriteLine("What index (0-4)?");
+            wrdIndex = Convert.ToInt16(Console.ReadLine());
+            if (wrdIndex >4 || wrdIndex < 0)
+            {
+                Console.WriteLine("It must be between 0 and 4");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            Console.WriteLine("The word at that index is: " + words[wrdIndex]);
+
+            Console.WriteLine("What index (0-5)?");
+            if (wrdIndex > 5 || wrdIndex < 0)
+            {
+                Console.WriteLine("It must be between 0 and 5");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            numIndex = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("The number at that index is: " + numbers[numIndex]);
+
+            Console.WriteLine("What index (0-3)?");
+            if (wrdIndex > 3 || wrdIndex < 0)
+            {
+                Console.WriteLine("It must be between 0 and 3");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            numIndex = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("The word in the list at that index is: " + stList[listIndex]);
+
+            Console.ReadKey();
+        }
+    }
+}
--- StringProgram.cs
+++ StringProgram.cs
@@ -7,28 +7,12 @@ namespace StringProgram
     {
         static void Main()
         {
-            StringBuilder sb = new StringBuilder("");
+            String[] words = { "cobble", "wobble", "bobble", "tobble", "mobble" };
+            int[] numbers = { 7, 22, 83, 63, 21, 3 };
+            int wrdIndex = 0;
+            int numIndex = 0;
 
-            Console.WriteLine("What is your first name: ");
-            String firstName = (Console.ReadLine());
-            Console.WriteLine("What is your middle name: ");
-            String middleName = (Console.ReadLine());
-            Console.WriteLine("What is your last name: ");
-            String lastName = (Console.ReadLine());
-            String fullName = firstName + " " + middleName + " " + lastName;
-            Console.WriteLine("So your full name is: " + fullName + "\n");
 
-            sb.AppendFormat("My name is {0} and I have been quiet for too long. ", fullName);
-            Console.WriteLine("What is your favorite battle cry: ");
-            String battleCry = (Console.ReadLine().ToUpper());
-            
-            Console.WriteLine("So you like to yell: " + battleCry);
-            Console.ReadKey();
-
-            sb.AppendFormat("The injustice of our people will no longer stand. ");
-            sb.AppendFormat("We want to watch Game of Thrones without having to subscribe to HBO. ");
-            sb.AppendFormat("Upon this hill we fight and die for our principles with an anguished yell of \"{0}\"!!!", battleCry);
-            Console.WriteLine(sb);
 
             Console.ReadKey();
         }

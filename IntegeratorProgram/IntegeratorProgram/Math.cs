0d60459d2f782ce864e75120ce8534f0f46a9479
Tyler <hybound@hotmail.com>
2019-05-09 16:59:58 -0600

    Create Integerator Program
    This program uses classes and methods to do math on an input by the user.

---
+++ IntegeratorProgram/IntegeratorProgram/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegeratorProgram
+{
+    class Math
+    {
+        public int Add10(int number)
+        {
+            int adder = number + 10;
+            return adder;
+        }
+
+        public int Mult10(int number)
+        {
+            int mult = number * 10;
+            return mult;
+        }
+
+        public int Div10(int number)
+        {
+            int div = number / 10;
+            return div;
+        }
+    }
+}
---
+++ IntegeratorProgram/IntegeratorProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegeratorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Type a number to add, multiply and divide by ten: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int num1 = math.Add10(number);
+            int num2 = math.Mult10(number);
+            int num3 = math.Div10(number);
+
+            Console.WriteLine("Your answers are: " + num1 + " and " + num2 + " and " + num3 + ", respectively.");
+            Console.ReadLine();
+        }
+    }
+}

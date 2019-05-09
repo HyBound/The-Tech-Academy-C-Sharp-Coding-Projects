e667808b5b7e5fad6191eee1608cab8c5ab04caf
Tyler <hybound@hotmail.com>
2019-05-09 17:49:38 -0600

    Create Integerator2

---
+++ Integerator2/Integerator2/Mathing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integerator2
+{
+    public class Mathing
+    {
+        public void Maths(int num1, int num2)
+        {
+            int result = num1 + 38 / 2;
+            Console.WriteLine("The second number is {0}", num2);
+        }
+    }
+}
---
+++ Integerator2/Integerator2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integerator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Give me a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give me a second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Mathing math = new Mathing();
+            math.Maths(num1, num2);
+            Console.ReadKey();
+            
+        }
+    }
+}

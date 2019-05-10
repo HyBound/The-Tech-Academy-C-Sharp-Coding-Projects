a58b35372c5ef308409568b784bde276494c5dd9
Tyler <hybound@hotmail.com>
2019-05-10 17:23:25 -0600

    Create Abstractor

---
+++ Abstractor/Abstractor/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheretor
+{
+    class Employee : Person
+    {
+        public int id = 0;
+        
+    }
+}

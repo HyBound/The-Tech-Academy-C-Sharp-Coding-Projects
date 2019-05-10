e2dfc5fa1f5500be0a54027cb63595b49525266b
Tyler <hybound@hotmail.com>
2019-05-10 15:46:13 -0600

    Create Inheretor

---
+++ Inheretor/Inheretor/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheretor
+{
+    class Person
+    {
+        public string FirstName = "";
+        public string LastName = "";
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
---
+++ Inheretor/Inheretor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheretor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sample = new Employee() { FirstName = "Sample", LastName = "Name" };
+            sample.SayName();
+            Console.ReadKey();
+        }
+    }
+}

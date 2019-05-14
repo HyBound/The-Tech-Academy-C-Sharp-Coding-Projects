0213ead2c1b2465b58c9c7941f50bbb80a0e7393
Tyler <hybound@hotmail.com>
2019-05-13 18:32:19 -0600

    Create Generics

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheretor
+{
+    public class Employee<T> : Person, IQuittable
+    {
+        public int id = 0;
+        public List<T> things = new List<T>();
+        public void Quit(Person person)
+        {
+            Environment.Exit(id);
+        }
+
+        public void SayThings(Employee<T> employee)
+        {
+            foreach (var thing in things)
+            {
+                Console.WriteLine(thing.ToString());
+            }
+        }
+
+        public static Boolean operator== (Employee<T> employee, Employee<T> employee2)
+        {
+            if (employee.id == employee2.id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static Boolean operator!= (Employee<T> employee, Employee<T> employee2)
+        {
+            if (employee.id == employee2.id)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheretor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> sample = new Employee<string>() { FirstName = "Sample", LastName = "Student", things = { "pants", "shirt", "shoes" } };
+            Employee<int> sample2 = new Employee<int>() { id=1, FirstName = "Tyler", LastName = "Student", things = { 2,8,17,32} };
+            sample.SayThings(sample);
+            sample2.SayThings(sample2);
+            Console.WriteLine(sample.id + " and " + sample2.id);
+
+            Console.ReadLine();
+
+            //Console.WriteLine("Type the letter 'q' to quit: ");
+            //string q = Convert.ToString(Console.ReadLine());
+            //if (q == "q")
+            //{
+            //    quitter.Quit(sample);
+            //}
+            
+            Console.ReadKey();
+        }
+    }
+}

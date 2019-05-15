dde446682adf4090a602b11cd700250daf5d9ca1
Tyler <hybound@hotmail.com>
2019-05-15 14:38:38 -0600

    Create Struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 6.8m;
+            Console.WriteLine("My number is " + num1.Amount);
+            Console.ReadLine();
+
+        }
+        public struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}

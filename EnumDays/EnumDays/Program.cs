c8f2ef6779b01b0e2216bd71c656d597d17a2531
Tyler <hybound@hotmail.com>
2019-05-15 11:21:13 -0600

    Create EnumDays

---
+++ EnumDays/EnumDays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a day of the week: ");
+                string day = Convert.ToString(Console.ReadLine());
+                eDays.Days eDay = (eDays.Days) Enum.Parse(typeof(eDays.Days), day);
+
+                Console.WriteLine(eDay + " is a day of the week");
+                Console.ReadLine();
+
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+            
+        }
+
+    }
+}
---
+++ EnumDays/EnumDays/eDays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDays
+{
+    public class eDays
+    {
+        public enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}

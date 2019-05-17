6fe87e742005086eb85e30b10728c99ae78dfb49
Tyler <hybound@hotmail.com>
2019-05-17 15:35:55 -0600

    Create Constants

---
+++ Constants/Constants/Boats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    class Boats
+    {
+        public int _boatId { get; set; }
+        public string _name { get; set; }
+
+        public Boats(int BoatId) : this(BoatId, "")
+        {
+
+        }
+        public Boats(int BoatId, string Name)
+        {
+            _boatId = BoatId;
+            _name = Name;
+        }
+    }
+}
---
+++ Constants/Constants/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Tyler";
+            var boat = "The Seaward";
+
+        }
+    }
+}

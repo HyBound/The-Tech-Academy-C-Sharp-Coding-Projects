2e33cc867690ee12c9e001e19ff09059191fbe12
Tyler <hybound@hotmail.com>
2019-05-16 18:57:25 -0600

    Create DateTime

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine("Give me a number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            DateTime future = now.AddHours(number);
+            Console.WriteLine("It will be {0}, {1} hours from now", future, number);
+            Console.ReadLine();
+        }
+    }
+}

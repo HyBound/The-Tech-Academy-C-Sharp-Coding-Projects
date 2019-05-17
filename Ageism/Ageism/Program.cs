8e2c500f9e641fb1f4a5e5cecac834ac535802c3
Tyler <hybound@hotmail.com>
2019-05-17 16:31:15 -0600

    Create Ageism

---
+++ Ageism/Ageism/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ageism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                bool isSuccess = false;
+                int age = 0;
+                while (!isSuccess)
+                {
+                    Console.WriteLine("What is your age: ");
+                    isSuccess = int.TryParse(Console.ReadLine(), out age);
+                    if (!isSuccess) Console.WriteLine("Please enter a real number.");
+                }
+                if (age < 0) throw new ArgumentException();
+                
+                var year = DateTime.Now.Year;
+                var zeroCheck = year / age;
+                var yearBorn = year - age;
+                Console.WriteLine("{0} was the year you were born. And now I feel old.", yearBorn);
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Try again, Please enter a real number.");
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You're not 0 years old! How would you be typing on a computer...");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Way to go, you failed at the simplest task...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
